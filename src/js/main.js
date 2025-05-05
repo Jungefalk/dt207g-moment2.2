e946a17681bed8c69355a293cce970a0b741ebb9
Caroline <c.jungefalk@gmail.com>
2025-05-05 10:25:55 +0200

    Lägg till skal till funktionerna för tydlig struktur

--- src/js/main.js
+++ src/js/main.js
@@ -10,3 +10,44 @@ let endDateEl = document.getElementById("end_date");
 let descriptionEl = document.getElementById("description");
 let buttonEl = document.getElementById("button");
 
+//Händelselyssnare
+window.addEventListener("load", init);
+buttonEl.addEventListener("click", postData)
+
+//Init
+function init() {
+
+    getData();
+    console.log("Sidan har laddat in");
+};
+
+
+//Hämta data
+function getData() {
+
+    console.log("datan har hämtats");
+
+    readData();
+}
+
+//Läs ut data
+function readData() {
+
+    console.log("Datan har lästs ut till skärmen")
+}
+
+//Posta data
+function postData() {
+
+    console.log("Datan har postats");
+
+    fetchData();
+}
+
+//Ta bort data
+function deleteData() {
+
+    console.log("Datan har raderats");
+
+    fetchData();
+}
