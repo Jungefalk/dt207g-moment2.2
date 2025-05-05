316812719016d2ebd0fbe348090bdefabd32d19b
Caroline <c.jungefalk@gmail.com>
2025-05-05 09:21:37 +0200

    Hämtat in html-element to

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+
+
+//Hämta id:n
+let tableEl = document.getElementById("work_experience");
+let companyNameEl = document.getElementById("company_name");
+let jobTitleEl = document.getElementById("job_title");
+let locationEl = document.getElementById("location");
+let startDateEl = document.getElementById("start_date")
+let endDateEl = document.getElementById("end_date");
+let descriptionEl = document.getElementById("description");
+let buttonEl = document.getElementById("button");
+
